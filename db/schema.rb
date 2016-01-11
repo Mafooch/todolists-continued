f337915b2c8bb457f330d80c5b199d628ca50998
Mafooch <joshuamaffuccio@gmail.com>
2016-01-11 13:35:40 -0500

    Add TodoList model and setup many to one with User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111031413) do
+ActiveRecord::Schema.define(version: 20160111183211) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20160111031413) do
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string   "list_name"
+    t.date     "list_due_date"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "user_id"
+  end
+
+  add_index "todo_lists", ["user_id"], name: "index_todo_lists_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
